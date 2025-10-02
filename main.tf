89a4bc9bf9720803789b150df64761597576020a
TEK Student <TEKstudent@vlab.lan>
2025-10-02 12:12:55 -0700

    Set unencrypt

--- main.tf
+++ main.tf
@@ -64,15 +64,15 @@ module "lb_security_group" {
   description = "Security group for load balancer with HTTP ports open within VPC"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks = ["10.0.0.0/16"]
   ingress_rules       = ["ssh-tcp"]
   ingress_with_cidr_blocks = [
     {
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
-      description = "SSH open to the world"
-      cidr_blocks = "0.0.0.0/0"
+      description = "SSH restricted to internal network"
+      cidr_blocks = "10.0.0.0/16"
     }
   ]
 
@@ -81,7 +81,6 @@ module "lb_security_group" {
     environment = "development"
   }
 }
-
 resource "random_string" "lb_id" {
   length  = 3
   special = false
@@ -140,5 +139,5 @@ module "ec2_instances" {
 resource "aws_ebs_volume" "unencrypted" {
   availability_zone = "us-west-1a"
   size              = 8
-  encrypted         = false # Intentional violation: unencrypted EBS volume
+  encrypted         = true # Remediated: enable encryption
 }
